0ba5e43c65a6c70b6b366941ba014199b42ae745
ftd10822390 <ftd10822390@dseu.ac.in>
2026-07-03 17:25:32 +0530

    script.js

--- script.js
+++ script.js
@@ -1,30 +1,12 @@
-let musicStarted = false;
 
-function startMusic() {
-    if (musicStarted) return;
+document.getElementById("startBtn").addEventListener("click", function () {
 
     let music = document.getElementById("bgMusic");
 
-    if (music) {
-        music.volume = 1;
-        music.play()
-        .then(() => {
-            console.log("Music playing");
-            musicStarted = true;
-        })
-        .catch((err) => {
-            console.log("Music blocked:", err);
-        });
-    }
-}
+    music.play().then(() => {
+        document.getElementById("startBtn").style.display = "none";
+    }).catch((err) => {
+        console.log(err);
+    });
 
-// multiple triggers for safety (mobile + desktop)
-document.addEventListener("click", startMusic);
-document.addEventListener("touchstart", startMusic);
-document.addEventListener("scroll", startMusic);
-
-
-// OPTIONAL: debug check
-window.addEventListener("load", () => {
-    console.log("Page loaded");
 });
